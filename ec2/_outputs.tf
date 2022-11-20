3b18290759230b60845c3fa93668e041fb52a696
chiho <chiholee@amazon.com>
2022-11-20 21:24:32 +0900

    modify

--- ec2/_outputs.tf
+++ ec2/_outputs.tf
@@ -1,3 +1,11 @@
 output "sg_id" {
   value = aws_security_group.this.id
 }
+
+output "public_ip" {
+  value = module.aws_instance.this.public_ip
+}
+
+output "private_dns" {
+  value = module.aws_instance.this.private_dns
+}
